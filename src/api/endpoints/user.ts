c4b123f72865633dc7ae06400ca1657f547f4706
Matti <25400788+MathiasDevelopes@users.noreply.github.com>
2025-12-17 12:54:04 +0100

    Fix API endpoint path for getCurrentUser method

--- src/api/endpoints/user.ts
+++ src/api/endpoints/user.ts
@@ -8,7 +8,7 @@ import { Endpoint } from "../endpoint";
 
 export class UserApi extends Endpoint {
   async getCurrentUser(): Promise<User> {
-    return this.client.getWithSchema("/permissions/user", UserSchema);
+    return this.client.getWithSchema("permissions/user", UserSchema);
   }
 
   async getPersonalInfo(): Promise<PersonalInfo> {
