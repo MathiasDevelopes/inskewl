7a4f5e3190e5d79a5de24869cc6df4c6f06e9b02
Matti <25400788+MathiasDevelopes@users.noreply.github.com>
2026-05-20 13:54:33 +0200

    docs: add comment for clarification

--- src/api/endpoints/tenant.ts
+++ src/api/endpoints/tenant.ts
@@ -4,6 +4,7 @@ import { Endpoint } from "../endpoint";
 
 export class TenantApi extends Endpoint {
   // This endpoint is publicly accessible and requires no authentication.
+  // NB: This has to be called from a tenant's url, not callable from the base url "inschool.visma.no"
   async getTenantlist(): Promise<Tenant[]> {
     return this.client.getWithSchema("tenant/list", z.array(TenantSchema));
   }
